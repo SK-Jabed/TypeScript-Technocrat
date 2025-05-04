31ff66b67c62395fdd868b80b1f60f27585d174d
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-04 22:13:45 +0600

    Data Types Related Acrivities Added

---
+++ module-1/src/data-types.ts
@@ -0,0 +1,41 @@
+{
+  // Basic Data Types
+
+  // ----- String -----
+  let firstName = "Sheikh"; // Impressive Type Infer
+  let lastName: string = "Jabed"; // Expressive Type Define
+
+  // ----- Number -----
+  let roll: number = 658840;
+
+  // ----- Boolean -----
+  let isGraduate: boolean = false;
+
+  // ----- Undefined -----
+  let x: undefined = undefined;
+
+  // ----- Null -----
+  let y: null = null;
+
+  let any;
+
+  //   let d: number;
+
+  //   d = 123;
+
+  //   // Array
+
+  //   let friends: string[] = ["rachel", "monica"];
+
+  //   let eligibleRollList: number[] = [1, 3];
+
+  //   eligibleRollList.push("dd");
+
+  //   // tuple --> array --> order --> type of values
+
+  //   let coordinates: [number, number] = [1, 5];
+
+  //   let ageName: [number, string, boolean] = [50, "Mr.X", true];
+
+  //   ageName[0] = 67;
+}
