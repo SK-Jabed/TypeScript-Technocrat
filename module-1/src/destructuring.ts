336c485358d95e3862749022357c8db2364e39c6
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-05 23:41:07 +0600

    New Functins and activities added on server side

--- module-1/src/destructuring.ts
+++ module-1/src/destructuring.ts
@@ -1,12 +1,19 @@
 let numbers: number[] = [2,3,4,5,6];
+
 type user = {
     name: string;
     age: number;
     email: string;
 }
 
-const user: user = {
+const user1: user = {
     name: "Neymar Jr.",
-    age: 16,
+    age: 32,
     email: "njr@gmail.com"
 }
+
+const user2: user = {
+  name: "Mohammed Salah",
+  age: 34,
+  email: "mosalah@gmail.com",
+};
