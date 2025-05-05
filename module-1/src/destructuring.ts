ca24e8fe6ba4a896dabd533839ea63451e385e7f
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-05 23:34:23 +0600

    New File Called Destructuring of TypeScript Added

---
+++ module-1/src/destructuring.ts
@@ -0,0 +1,12 @@
+let numbers: number[] = [2,3,4,5,6];
+type user = {
+    name: string;
+    age: number;
+    email: string;
+}
+
+const user: user = {
+    name: "Neymar Jr.",
+    age: 16,
+    email: "njr@gmail.com"
+}
