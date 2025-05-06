2c859d2e465951c8ffcb5ed11e01165e9803f40e
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-06 22:57:35 +0600

    Worked on Object Oriented TypeScript and Activities Related to These Completed

--- module-1/src/mapped-types.ts
+++ module-1/src/mapped-types.ts
@@ -7,5 +7,28 @@
   );
 
   console.log(arrOfStrings);
+
+  type AreaNumber = {
+    height: number;
+    width: number;
+  };
+
+  type Height = AreaNumber["height"]; // look up type
+  // type AreaString = {
+  //   height: string;
+  //   width: string
+  // }
+  // keyof AreaNumber => "height"|"width"
+
+  // T => {height:string;width:number}
+  // key => T["width"]
+  type AreaString<T> = {
+    [key in keyof T]: T[key];
+  };
+
+  const area1: AreaString<{ height: string; width: number }> = {
+    height: "100",
+    width: 50,
+  };
   //
 }
