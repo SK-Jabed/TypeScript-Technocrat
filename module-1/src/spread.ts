aeab311e9d0152f22c35f4d21bacd31da612ee85
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-06 21:34:36 +0600

    New Functionalities and New Activities Added

--- module-1/src/spread.ts
+++ module-1/src/spread.ts
@@ -1,10 +1,12 @@
-type player = {
-    name: string;
-    age: number;
-    jerseyNumber?: number
-}
+{
+    type player = {
+      name: string;
+      age: number;
+      jerseyNumber?: number;
+    };
 
-const leo: player = {
-    name: "Leo Messi",
-    age: 37,
+    const leo: player = {
+      name: "Leo Messi",
+      age: 37,
+    };
 }
