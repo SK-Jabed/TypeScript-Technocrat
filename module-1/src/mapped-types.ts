45e007f63d424d2a1fecabc3aad94f8f101c81f8
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-06 22:04:49 +0600

    Mapped Types related activities added

---
+++ module-1/src/mapped-types.ts
@@ -0,0 +1,11 @@
+{
+  //
+  const arrOfNumbers: number[] = [5, 10, 15, 20];
+
+  const arrOfStrings: string[] = arrOfNumbers.map((number) =>
+    number.toString()
+  );
+
+  console.log(arrOfStrings);
+  //
+}
