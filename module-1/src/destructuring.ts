bbb32ed1a30b9b3ea96bffa9a69a35ff47e05ffd
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-06 21:34:54 +0600

    New Functionalities and New Activities Added

--- module-1/src/destructuring.ts
+++ module-1/src/destructuring.ts
@@ -1,19 +1,21 @@
-let numbers: number[] = [2,3,4,5,6];
+{
+  let numbers: number[] = [2, 3, 4, 5, 6];
 
-type user = {
+  type user = {
     name: string;
     age: number;
     email: string;
-}
+  };
 
-const user1: user = {
+  const user1: user = {
     name: "Neymar Jr.",
     age: 32,
-    email: "njr@gmail.com"
-}
+    email: "njr@gmail.com",
+  };
 
-const user2: user = {
-  name: "Mohammed Salah",
-  age: 34,
-  email: "mosalah@gmail.com",
-};
+  const user2: user = {
+    name: "Mohammed Salah",
+    age: 34,
+    email: "mosalah@gmail.com",
+  };
+}
