bb38a30f6c8fcf57570ef2d3de674653a17e45c2
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-13 23:21:50 +0600

    New Function Added and Data-Types Related Activities Done

--- module-1/src/class-object.ts
+++ module-1/src/class-object.ts
@@ -1,20 +1,20 @@
 {
-    // 
-      class Animal {
-        constructor(
-          public name: string,
-          public species: string,
-          public sound: string
-        ) {}
+  //
+  class Animal {
+    constructor(
+      public name: string,
+      public species: string,
+      public sound: string
+    ) {}
 
-        makeSound() {
-          console.log(`The ${this.name} says ${this.sound}`);
-        }
-      }
+    makeSound() {
+      console.log(`The ${this.name} says ${this.sound}`);
+    }
+  }
 
-      const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
-      const cat = new Animal("Pussy", "cat", "Meaw Meaw");
+  const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
+  const cat = new Animal("Pussy", "cat", "Meaw Meaw");
 
-      cat.makeSound();
-    // 
+  cat.makeSound();
+  //
 }
